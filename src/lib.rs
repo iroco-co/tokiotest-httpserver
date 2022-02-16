5e103a5d24e1d8040581014d462e1eaddeba5310
Bruno Thomas <bruno@barreverte.fr>
2022-02-16 18:46:22 +0100

    feat: run several servers on different ports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+use std::collections::BinaryHeap;
 use std::convert::Infallible;
 use std::future::Future;
-use std::net::SocketAddr;
+use std::net::{SocketAddr};
 use test_context::AsyncTestContext;
 use tokio::sync::oneshot::{Receiver, Sender};
 use tokio::task::JoinHandle;
@@ -8,14 +9,29 @@ use hyper::{Body, Client, Request, Server, StatusCode};
 use hyper::client::connect::dns::GaiResolver;
 use hyper::client::HttpConnector;
 use hyper::service::{make_service_fn, service_fn};
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 pub type Response = hyper::Response<hyper::Body>;
 pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+lazy_static! {
+    static ref PORTS: Mutex<BinaryHeap<u16>> = Mutex::new(BinaryHeap::from((12300u16..12400u16).collect::<Vec<u16>>()));
+}
+
+fn take_port() -> u16 {
+    PORTS.lock().unwrap().pop().unwrap()
+}
+
+fn release_port(port: u16) {
+    PORTS.lock().unwrap().push(port)
+}
 
 struct HttpTestContext {
     client: Client<HttpConnector<GaiResolver>, Body>,
     server_handler: JoinHandle<Result<(), hyper::Error>>,
     sender: Sender<()>,
+    port: u16
 }
 
 async fn handle(_req: Request<Body>) ->  Result<Response, Infallible> {
@@ -43,7 +59,8 @@ pub async fn run_service(
 #[async_trait::async_trait]
 impl AsyncTestContext for HttpTestContext {
     async fn setup() -> HttpTestContext {
-        let addr = "127.0.0.1:12345".parse().expect("address creation works");
+        let port = take_port();
+        let addr = SocketAddr::new("127.0.0.1".parse().unwrap(), port);
         let (sender, receiver) = tokio::sync::oneshot::channel::<()>();
         let server_handler = tokio::spawn(run_service(addr, receiver).await);
         let client = Client::new();
@@ -51,12 +68,14 @@ impl AsyncTestContext for HttpTestContext {
             client,
             server_handler,
             sender,
+            port
         }
     }
 
     async fn teardown(self) {
         let _ = self.sender.send(()).unwrap();
         let _ = tokio::join!(self.server_handler);
+        release_port(self.port)
     }
 }
 
@@ -69,8 +88,8 @@ mod test {
     #[test_context(HttpTestContext)]
     #[tokio::test]
     async fn test_get_without_expect_should_send_500(ctx: &mut HttpTestContext) {
-        let resp = ctx.client.get("http://localhost:12345".parse::<Uri>().unwrap()).await.unwrap();
+        let uri = format!("http://{}:{}", "localhost", ctx.port).parse::<Uri>().unwrap();
+        let resp = ctx.client.get(uri).await.unwrap();
         assert_eq!(500, resp.status());
     }
-
 }
