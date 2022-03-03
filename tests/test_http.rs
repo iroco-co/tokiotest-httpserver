d5c80710e0057c459006dd05b019b93f74697772
Bruno Thomas <bruno@barreverte.fr>
2022-03-03 11:42:49 +0100

    refactor: rename test url

--- tests/test_http.rs
+++ tests/test_http.rs
@@ -6,8 +6,8 @@ use test_context::test_context;
 #[test_context(HttpTestContext)]
 #[tokio::test]
 async fn test_get_respond_200(ctx: &mut HttpTestContext) {
-    let uri = format!("http://{}:{}/unknown", "localhost", ctx.port).parse::<Uri>().unwrap();
-    ctx.add(HandlerBuilder::new("/unknown").status_code(StatusCode::OK).build());
+    let uri = format!("http://{}:{}/ok", "localhost", ctx.port).parse::<Uri>().unwrap();
+    ctx.add(HandlerBuilder::new("/ok").status_code(StatusCode::OK).build());
 
     let resp = ctx.client.get(uri).await.unwrap();
 
