4f473947fd3358100cb56f8f5442e8443e97f7bd
Bruno Thomas <bruno@barreverte.fr>
2022-03-03 11:40:54 +0100

    test: add a 200-OK test for documentation

---
+++ tests/test_http.rs
@@ -0,0 +1,15 @@
+use hyper::{StatusCode, Uri};
+use tokiotest_httpserver::handler::HandlerBuilder;
+use tokiotest_httpserver::HttpTestContext;
+use test_context::test_context;
+
+#[test_context(HttpTestContext)]
+#[tokio::test]
+async fn test_get_respond_200(ctx: &mut HttpTestContext) {
+    let uri = format!("http://{}:{}/unknown", "localhost", ctx.port).parse::<Uri>().unwrap();
+    ctx.add(HandlerBuilder::new("/unknown").status_code(StatusCode::OK).build());
+
+    let resp = ctx.client.get(uri).await.unwrap();
+
+    assert_eq!(200, resp.status());
+}
